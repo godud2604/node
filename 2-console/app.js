fde8c3e5746f5756e946514835f8ce5cd27fea4f
godud2604 <ees238@naver.com>
2022-05-06 02:33:18 +0900

    2-console

---
+++ 2-console/app.js
@@ -0,0 +1,47 @@
+console.log('logging....');
+
+// log level
+console.log('log'); // 개발
+console.info('info'); // 정보
+console.warn('warn'); // 경보
+console.error('error'); // 에러, 사용자 에러, 시스템 에러
+
+// assert 
+// => true가 아닌 경우에만 출력
+console.assert(2 === 3, 'not same!'); // 출력
+console.assert(2 === 2, 'same!'); // 출력 x
+
+// print object
+const student = { name: 'hy', age: 20, company: { name: 'AC' } };
+console.log(student);
+console.table(student);
+console.dir(student, { showHidden: true, colors: false, depth: 0 });
+
+// measuring time
+console.time('for loop');
+for (let i = 0; i < 10; i++) {
+  i++;
+}
+console.timeEnd('for loop');
+
+// counting
+function a() {
+  console.count('a function');
+}
+a();
+a();
+console.countReset('a function');
+a();
+
+// trace
+function f1() {
+  f2();
+}
+function f2() {
+  f3();
+}
+function f3() {
+  console.log('f3');
+  console.trace();
+}
+f1();
