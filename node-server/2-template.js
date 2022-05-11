f9fd1a3189d18a9c02a1e4709afaca9e4c18d09d
godud2604 <ees238@naver.com>
2022-05-11 17:58:59 +0900

    Using the template engine EJS

---
+++ node-server/2-template.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const fs = require('fs');
+// const http2 = require('http2'); // https
+const ejs = require('ejs');
+
+const name = 'hy';
+const courses = [{ name: 'HTML' }, { name: 'CSS' }, { name: 'JS' }];
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  res.setHeader('Content-Type', 'text/html');
+  if (url === '/') {
+    ejs
+      .renderFile('./template/index.ejs', { name })
+      .then(data => res.end(data));
+
+  } else if (url === '/courses') {
+    ejs
+      .renderFile('./template/courses.ejs', { courses })
+      .then(data => res.end(data));
+
+  } else {
+    ejs
+      .renderFile('./template/not-found.ejs', { name })
+      .then(data => res.end(data));
+}
+});
+
+server.listen(8080);
