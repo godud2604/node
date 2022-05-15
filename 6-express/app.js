5707d40a6eac8a680679657a99601ab0cdc8cfe2
godud2604 <ees238@naver.com>
2022-05-16 00:04:45 +0900

    11.6. middleware

--- 6-express/app.js
+++ 6-express/app.js
@@ -1,18 +1,38 @@
 import express from 'express';
 const app = express();
 
+app.all('/api/*', (req, res, next) => {
+  console.log('all');
+  next();
+});
+app.use('/sky', (req, res, next) => {
+  console.log('use');
+  next();
+});
+app.get(
+  '/',
+  (req, res, next) => {
+    console.log('first');
+    // next(new Error('error')); // error 발생
+    res.send('hello'); // 뒤에 연결된 미들웨어는 호출되지 않는다.
+  },
+  (req, res, next) => {
+    console.log('first2');
+    next();
+  },
+);
 
-app.get('/sky/:id', (req, res, next) => {
-  console.log('get');
-  console.log(req.path);
-  // console.log(req.headers);
-  console.log(req.params.id);
-  console.log(req.query.keyword);
+app.get('/', (req, res, next) => {
+  console.log('second');
+});
 
-  // res.json({ name: 'Hy' });
-  res.setHeader('key', 'value');
-  res.status(201).send('created');
-  // res.sendStatus(400);
+// 처리할 수 없는 경로에 대한 상태
+app.use((req, res, next) => {
+  res.status(404).send('Not available! ~_~')
+});
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(500).send('Sorry, try later!'); 
 });
 
 app.listen(8080);
