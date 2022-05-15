f4794e5f2b8fc0d3e92f959dd89dfd14463ca84c
godud2604 <ees238@naver.com>
2022-05-16 00:23:49 +0900

    11.7. post

--- 6-express/app.js
+++ 6-express/app.js
@@ -1,38 +1,10 @@
 import express from 'express';
 const app = express();
 
-app.all('/api/*', (req, res, next) => {
-  console.log('all');
-  next();
-});
-app.use('/sky', (req, res, next) => {
-  console.log('use');
-  next();
-});
-app.get(
-  '/',
-  (req, res, next) => {
-    console.log('first');
-    // next(new Error('error')); // error 발생
-    res.send('hello'); // 뒤에 연결된 미들웨어는 호출되지 않는다.
-  },
-  (req, res, next) => {
-    console.log('first2');
-    next();
-  },
-);
+app.use(express.json());
 
-app.get('/', (req, res, next) => {
-  console.log('second');
-});
-
-// 처리할 수 없는 경로에 대한 상태
-app.use((req, res, next) => {
-  res.status(404).send('Not available! ~_~')
-});
-app.use((error, req, res, next) => {
-  console.error(error);
-  res.status(500).send('Sorry, try later!'); 
+app.post('/', (req, res, next) => {
+  console.log(req.body);
 });
 
 app.listen(8080);
