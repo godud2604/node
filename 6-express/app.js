152cd8792a1dedb867b59eb49b165bc2dad21035
godud2604 <ees238@naver.com>
2022-05-15 23:30:07 +0900

    11.4. request

---
+++ 6-express/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+const app = express();
+
+
+app.get('/sky/:id', (req, res, next) => {
+  console.log('get');
+  console.log(req.path);
+  // console.log(req.headers);
+  console.log(req.params.id);
+  console.log(req.query.keyword);
+  
+  res.send('hi!')
+});
+
+app.listen(8080);
