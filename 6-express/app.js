2154e026b7226cc70104e2e07041206f7dda91c6
godud2604 <ees238@naver.com>
2022-05-15 23:46:22 +0900

    11.5. response

--- 6-express/app.js
+++ 6-express/app.js
@@ -8,8 +8,11 @@ app.get('/sky/:id', (req, res, next) => {
   // console.log(req.headers);
   console.log(req.params.id);
   console.log(req.query.keyword);
-  
-  res.send('hi!')
+
+  // res.json({ name: 'Hy' });
+  res.setHeader('key', 'value');
+  res.status(201).send('created');
+  // res.sendStatus(400);
 });
 
 app.listen(8080);
