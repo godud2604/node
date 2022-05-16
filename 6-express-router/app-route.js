ce214d6176f2a20c54d35dd41844abf786b95012
godud2604 <ees238@naver.com>
2022-05-16 17:19:18 +0900

    11.11 express.urlencoded, express.static

--- 6-express-router/app-route.js
+++ 6-express-router/app-route.js
@@ -4,7 +4,20 @@ import userRouter from './router/user.js';
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // REST API -> Body
+app.use(express.urlencoded({ extended: false })); // HTML Form -> Body
+
+const options = {
+  dotfiles: 'ignore', // 숨겨진 파일은 보여지지 않도록
+  etag: false,
+  index: false,
+  maxAge: '1d', // 캐시는 얼마나 지속될건지
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Data.now());
+  },
+};
+app.use(express.static('public', options));
 
 app.use('/posts', postRouter);
 app.use('/users', userRouter);
